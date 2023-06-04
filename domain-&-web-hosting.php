8a0a1d48a578f7442b3ad0deff40a5c6cd2c1936
nuaiman03 <nuaiman03fahim@gmail.com>
2023-06-05 01:50:23 +0600

    al changed

--- domain-&-web-hosting.php
+++ domain-&-web-hosting.php
@@ -6,22 +6,345 @@
       <div class="container " data-aos="fade-up">
           <div class="col content" data-aos="fade-right" data-aos-delay="100">
             <div class="heading-h text-center">
-                <h3>Domain & Hosting</h3>
-                <h4>Begin the Search for Your Perfect Domain Name...</h4>
+                <h3>Domain & Hosting</h3><br>
+                <h4 class="fst-italic">Begin the Search for Your Perfect Domain Name...</h4>
+                <h5 class="fst-italic">Select Hosting Your Plan Now</h5>
+                <p class="fst-italic">30 day Money Back Gurantee. No question asked.</p>   <br><br> 
+                <button type="button" class="btn btn-primary"><span class="bi bi-arrow-down"></span> Check Details <span class="bi bi-arrow-down"></span></button>
             </div><br>
-             
-            <p class="fst-italic">
-                Select Hosting Your Plan Now
-            </p>  
-            <p class="fst-italic">
-                30 day Money Back Gurantee. No question asked.
-            </p>    
-            <div class="heading-h text-center">
-            <button type="submit">Buy Now</button>
-            </div>        
+            
             
           </div>
       </div>
     </section><!-- End About Section -->
+
+
+<! ========================  
+    Syed Sahar Ali Raza      
+  ========================  !>
+<div id="generic_price_table">   
+    <section>
+        <div class="container">
+            
+            <!--BLOCK ROW START-->
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+            </div>  
+            <!--//BLOCK ROW END-->
+            <br>
+            <!--BLOCK ROW START-->
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+            </div>  
+            <!--//BLOCK ROW END-->
+            <br>
+            <!--BLOCK ROW START-->
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="generic_content clearfix"> 
+                        <div class="generic_head_price clearfix">
+                            <div class="generic_head_content clearfix"> 
+                                <div class="head_bg"></div>
+                                <div class="head">
+                                    <span>Basic</span>
+                                </div>     
+                            </div>
+                            <div class="generic_price_tag clearfix">  
+                                <span class="price">
+                                    <span class="sign">$</span>
+                                    <span class="currency">99</span>
+                                    <span class="cent">.99</span>
+                                    <span class="month">/MON</span>
+                                </span>
+                            </div>
+                        </div>                            
+                        <div class="generic_feature_list">
+                          <ul>
+                              <li><span>2GB</span> Bandwidth</li>
+                                <li><span>150GB</span> Storage</li>
+                                <li><span>12</span> Accounts</li>
+                                <li><span>7</span> Host Domain</li>
+                                <li><span>24/7</span> Support</li>
+                            </ul>
+                        </div>
+                        <div class="generic_price_btn clearfix">
+                          <a class="" href="">Sign up</a>
+                        </div>
+                    </div>  
+                </div>
+
+            </div>  
+            <!--//BLOCK ROW END-->
+            
+        </div>
+    </section>             
+</div>
+
+
 
 <?php require_once('main/footer.php'); ?>
