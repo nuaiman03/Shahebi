f1672fc98809217de1f530b6986571d044839d8a
nuaiman03 <nuaiman03fahim@gmail.com>
2023-06-04 10:48:51 +0600

    first commit

---
+++ main/header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Shahebi</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Gp
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.html"><div class="logo-container">
+                                <div class="logo-wrap local-scroll">
+                                  <a href="#home">
+                                    <img class="logo" src="assets/img/logo.png" alt="logo" data-rjs="2">
+                                  </a>
+                                </div>
+                            </div></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li class="dropdown"><a href="#"><span>Service</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#"><span>WEBSITE DEVELOPMENT</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">Dynamic Website</a></li>
+                    <li><a href="#">PSD to Static Website</a></li>
+                    <li><a href="#">Ecommerce Website</a></li>
+                    <li><a href="#">Website Support</a></li>
+                    <li><a href="#">SSL & Cyber Security</a></li>
+                    <li><a href="#">Domain & Web Hosting</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>MOBILE APP</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">Apps Development</a></li>
+                    <li><a href="#">App Promotion</a></li>
+                    <li><a href="#">Apps Support</a></li>
+                    <li><a href="#">Convert Website</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>SOFTWARE DEVELOPMENT</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">Inventory System</a></li>
+                    <li><a href="#">School Management</a></li>
+                    <li><a href="#">Custom Build Software</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>DIGITAL MARKETING</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">SEO Services</a></li>
+                    <li><a href="#">Facebook Marketing</a></li>
+                    <li><a href="#">Google AdWords Marketing</a></li>
+                    <li><a href="#">YouTube Marketing</a></li>
+                    <li><a href="#">SMS Marketing & Data</a></li>
+                    <li><a href="#">Example</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>GRAPHICS DESIGN</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">Clipping Path Service</a></li>
+                    <li><a href="#">Logo Design</a></li>
+                    <li><a href="#">Banner Design</a></li>
+                    <li><a href="#">Business Card</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>VIDEO PRODUCTION</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                    <li><a href="#">Example</a></li>
+                    <li><a href="#">Example</a></li>
+                    <li><a href="#">Example</a></li>
+                </ul>
+              </li>
+              
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#about" class="get-started-btn scrollto">Get Started</a>
+
+    </div>
+  </header><!-- End Header -->
