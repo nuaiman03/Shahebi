410b7979c79f9881072b787b2ca45dd1aca9afa8
nuaiman03 <nuaiman03fahim@gmail.com>
2023-06-17 17:07:24 +0600

    update 26

--- dynamic-website.php
+++ dynamic-website.php
@@ -1,15 +1,14 @@
 <?php require_once('./main/header.php'); ?>
 
-<!-- ======= Dy Website Headding Section ======= -->
+<!-- ======= Dy Website Hero Section ======= -->
 <section id="dy-web-hero" class="dy-web-hero" style="padding-top: 10%;">
     <div class="container" data-aos="fade-up">
-
         <div class="row content">
             <div class="col-lg-6" data-aos="fade-right">
                 <h2>Dynamic website Development</h2>
                 <h3>Why You Should Develop A Dynamic Website For Your Company?</h3>
                 <br>
-                <a href="#about" class="get-started-btn scrollto">See Package & Price</a>
+                <a href="#pricing-1" class="get-started-btn scrollto">See Package & Price</a>
             </div>
             <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
                 <ul>
@@ -30,23 +29,18 @@
                 </ul>
             </div>
         </div>
-
     </div>
-</section><!-- End About Us Section -->
+</section><!-- End Dy Website Hero Section -->
 
-<!-- Services Section - Home Page -->
-<section id="services" class="services-1">
-
+<!-- Dy Website Provide Section -->
+<section id="dy-web-provide" class="dy-web-provide">
     <div class="container">
-
         <div class="row gy-4">
-
             <div data-aos-delay="100" data-aos="fade-up" class="col-lg-6">
                 <div class="service-item d-flex">
                     <div class="icon flex-shrink-0"><i class="bi bi-wordpress"></i></div>
                     <div>
-                        <h4 class="title"><a class="stretched-link" href="services-details.html">WordPress Website
-                                Development</a></h4>
+                        <h4 class="title">WordPress Website Development</h4>
                         <p class="description">We offer a complete WordPress website design and development that
                             includes branding, SEO friendly, and a focus on visitor conversions. WordPress is the best
                             web software which you can use to create a highly functional website with your requirements.
@@ -54,36 +48,29 @@
                     </div>
                 </div>
             </div>
-
             <div data-aos-delay="200" data-aos="fade-up" class="col-lg-6">
                 <div class="service-item d-flex">
                     <div class="icon flex-shrink-0"><i class="bi bi-braces"></i></div>
                     <div>
-                        <h4 class="title"><a class="stretched-link" href="services-details.html">Custom Website
-                                Development</a></h4>
+                        <h4 class="title">Custom Website Development</h4>
                         <p class="description">If you need to take your website to the next level so that We could take
                             each client through a creative process to build a website that reflects their brand and
                             generates sales. It will be an easy process by doing it together.</p>
                     </div>
                 </div>
             </div>
-
         </div>
-
     </div>
+</section><!-- End Dy Website Services Section -->
 
-</section><!-- End Services Section -->
-
-<!-- ======= Features Section ======= -->
-<section id="features" class="features">
-
+<!-- ======= Dy Website Problem Section ======= -->
+<section id="dy-web-problem" class="dy-web-problem">
     <div class="container" data-aos="fade-up">
-        <!-- Feature Tabs -->
         <div class="col">
-            <div class="heading-h text-center">
-                <h3>Why Most Websites Fail</h3>
+            <div class="section-title">
+                <h2>Why</h2>
+                <p>Most Websites Fail</p>
             </div>
-
             <!-- Tabs -->
             <ul class="nav nav-pills mb-5">
                 <li>
@@ -99,10 +86,8 @@
                     <a class="nav-link" data-bs-toggle="pill" href="#tab4">Most Web ignore mobile acceptance</a>
                 </li>
             </ul><!-- End Tabs -->
-
             <!-- Tab Content -->
             <div class="tab-content">
-
                 <!-- Start Tab 1 Content -->
                 <div class="tab-pane fade show active" id="tab1">
                     <div class="row justify-content-center">
@@ -118,7 +103,6 @@
                     </div>
                 </div>
                 <!-- End Tab 1 Content -->
-
                 <!-- Start Tab 2 Content -->
                 <div class="tab-pane fade show" id="tab2">
                     <div class="row justify-content-center">
@@ -136,7 +120,6 @@
                     </div>
                 </div>
                 <!-- End Tab 2 Content -->
-
                 <!-- Start Tab 3 Content -->
                 <div class="tab-pane fade show" id="tab3">
                     <div class="row justify-content-center">
@@ -153,7 +136,6 @@
                     </div>
                 </div>
                 <!-- End Tab 3 Content -->
-
                 <!-- Start Tab 4 Content -->
                 <div class="tab-pane fade show" id="tab4">
                     <div class="row justify-content-center">
@@ -170,89 +152,81 @@
                     </div>
                 </div>
                 <!-- End Tab 4 Content -->
-
-
-
-            </div>
-
-        </div>
-    </div><!-- End Feature Tabs -->
-
-    <!-- Feature Icons -->
-    <div class="row justify-content-center feature-icons" style="margin-top:120px;" data-aos="fade-up">
-        <h3>Ratione mollitia eos ab laudantium rerum beatae quo</h3>
-
-        <div class="col-xl-8 content">
-            <div class="row align-self-center gy-4">
-
-                <div class="col-md-6 icon-box" data-aos="fade-up">
-                    <i class="bi bi-wordpress"></i>
-                    <div>
-                        <h4>Intact Foundation</h4>
-                        <p>We always try to build our sites on search-friendly and flexible WordPress or others CMS,
-                            incorporating HTML5 markup to prepare you for future web standards and microdata that
-                            increases your visibility for search engines.</p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-                    <i class="bi bi-tools"></i>
-                    <div>
-                        <h4>Easy to Maintain</h4>
-                        <p>We always try to build your site using best CMS WordPress or custom which changing and
-                            updating your site is in all point-and-click. You don’t need to be a developer to make
-                            changes and get the amazing experience with your website.</p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-                    <i class="bi bi-phone-flip"></i>
-                    <div>
-                        <h4>Mobile Responsive</h4>
-                        <p>Your site will look amazing and user-friendly no matter what type of device your customers
-                            are viewing it on. Our designers always trying to build such designs which are dynamically
-                            adjustable for any tablet and smartphone users which should be profitable for your site.</p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-                    <i class="bi bi-graph-up"></i>
-                    <div>
-                        <h4>Built-in Optimization</h4>
-                        <p>We have effective search optimization experts and we make sure that your site will not only
-                            be found but also stay competitive in search engines as well.</p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-                    <i class="bi bi-person-gear"></i>
-                    <div>
-                        <h4>Life Time Web Support</h4>
-                        <p>arge iT offers full support for your websites with our great managing team. Those who will
-                            constantly monitor and maintain your website on 24/7/365 in under terms and conditions.</p>
-                    </div>
-                </div>
-
-                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-                    <i class="bi bi-gear"></i>
-                    <div>
-                        <h4>Complete Free Tools</h4>
-                        <p>We include thousands of dollar plugins with each customer site. Unbelievably powerful
-                            software tools and website features will be added to you for free.</p>
-                    </div>
-                </div>
-
             </div>
         </div>
-
-    </div><!-- End Feature Icons -->
-
     </div>
+</section><!-- End Dy Website Problem Section -->
 
-</section><!-- End Features Section -->
+<!-- ======= Dy Website Srvices Section ======= -->
+<section id="dy-web-srvices" class="dy-web-services">
+    <div class="container" data-aos="fade-up">
+        <div class="row justify-content-center feature-icons" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Service</h2>
+                <p>You Should Know</p>
+            </div>
+            <div class="col-xl-8 content">
+                <div class="row align-self-center gy-4">
 
-<!-- ======= Pricing Section ======= -->
-<section id="pricing" class="pricing-1">
+                    <div class="col-md-6 icon-box" data-aos="fade-up">
+                        <i class="bi bi-wordpress"></i>
+                        <div>
+                            <h4>Intact Foundation</h4>
+                            <p>We always try to build our sites on search-friendly and flexible WordPress or others CMS,
+                                incorporating HTML5 markup to prepare you for future web standards and microdata that
+                                increases your visibility for search engines.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                        <i class="bi bi-tools"></i>
+                        <div>
+                            <h4>Easy to Maintain</h4>
+                            <p>We always try to build your site using best CMS WordPress or custom which changing and
+                                updating your site is in all point-and-click. You don’t need to be a developer to make
+                                changes and get the amazing experience with your website.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                        <i class="bi bi-phone-flip"></i>
+                        <div>
+                            <h4>Mobile Responsive</h4>
+                            <p>Your site will look amazing and user-friendly no matter what type of device your customers
+                                are viewing it on. Our designers always trying to build such designs which are dynamically
+                                adjustable for any tablet and smartphone users which should be profitable for your site.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+                        <i class="bi bi-graph-up"></i>
+                        <div>
+                            <h4>Built-in Optimization</h4>
+                            <p>We have effective search optimization experts and we make sure that your site will not only
+                                be found but also stay competitive in search engines as well.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+                        <i class="bi bi-person-gear"></i>
+                        <div>
+                            <h4>Life Time Web Support</h4>
+                            <p>arge iT offers full support for your websites with our great managing team. Those who will
+                                constantly monitor and maintain your website on 24/7/365 in under terms and conditions.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
+                        <i class="bi bi-gear"></i>
+                        <div>
+                            <h4>Complete Free Tools</h4>
+                            <p>We include thousands of dollar plugins with each customer site. Unbelievably powerful
+                                software tools and website features will be added to you for free.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- End Dy Website Srvices Section -->
+
+<!-- ======= Dy Website Pricing Section ======= -->
+<section id="dy-web-pricing" class="dy-web-pricing">
     <div class="container" data-aos="fade-up">
 
         <div class="row">
