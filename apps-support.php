70831265b3fd7e6b7bb25ab6224b2b759ee3eb31
nuaiman03 <nuaiman03fahim@gmail.com>
2023-06-19 15:10:44 +0600

    update 30

--- apps-support.php
+++ apps-support.php
@@ -11,7 +11,10 @@
             <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
                 <h5>Full Apps Support in Cheap Rate</h5>
                 <h2>Mobile App <span>Support</span> Services</h2>
-                <p>Shahebi Soft is a Professional Mobile Application Developing company in Bangladesh. We always makes sure that the apps are made in the highest of standard and high quality. We have dedicated developers that are well versed in developing mobile apps for iOS, Android, Blackberry, Windows Phone and more. For the most part, we provide mobile applications that are world class, user friendly, with high quality and apps that our clients will surely like. </p>
+                <p>Shahebi Soft is a Professional Mobile Application Developing company in Bangladesh. We always makes sure that the apps are made in the
+                    highest of standard and high quality. We have dedicated developers that are well versed in developing mobile apps for iOS, Android,
+                    Blackberry, Windows Phone and more. For the most part, we provide mobile applications that are world class, user friendly, with high quality
+                    and apps that our clients will surely like. </p>
                 <br>
                 <a href="#">Open Your Ticket</a>
             </div>
@@ -19,145 +22,97 @@
     </div>
 </section><!-- End Hero Section -->
 
-
 <!-- ======= Start Debugging Apps Section ======= -->
-<section id="about-us" class="about-us-1" style="background: white;">
+<section id="app-sup-debugging" class="app-sup-debugging">
     <div class="container" data-aos="fade-up">
         <div class="row content">
-            <div class="col-lg-7 " data-aos="fade-left">
-                <h2 style="color: black; font-weight: 900; text-transform: upercase; font-size: 30px;">Debugging Apps &
-                    Error Fixing Service</h2>
-                <p style="color: black;">There are lots of problems to develop an apps. Whatever your problem is, don’t
-                    worry, Just tell us what’s your problems and we’ll solve quickly! Not only that but we’ll help you
-                    learn and not have those kind of bugs again!</p>
+            <div class="col-lg-7" data-aos="fade-left">
+                <h2>Debugging Apps & Error Fixing Service</h2>
+                <p>There are lots of problems to develop an apps. Whatever your problem is, don’t worry, Just tell us what’s your problems and we’ll solve
+                    quickly! Not only that but we’ll help you learn and not have those kind of bugs again!</p>
                 <ul>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Bug Investigation & 2 Revisions</li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> JSON data, Retrofit API call issues
-                    </li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Pro guard & UI issues</li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Ad & Social API Integration Problems
-                    </li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Remove Suspends & any others issues
-                    </li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Highly Trusted & Most Secure
-                        Confirmation</li>
-                    <li style="color: black;"><i class="ri-check-double-line"></i> Free Consultation & 24/7 Support</li>
+                    <li><i class="ri-check-double-line"></i> Bug Investigation & 2 Revisions</li>
+                    <li><i class="ri-check-double-line"></i> JSON data, Retrofit API call issues</li>
+                    <li><i class="ri-check-double-line"></i> Pro guard & UI issues</li>
+                    <li><i class="ri-check-double-line"></i> Ad & Social API Integration Problems</li>
+                    <li><i class="ri-check-double-line"></i> Remove Suspends & any others issues</li>
+                    <li><i class="ri-check-double-line"></i> Highly Trusted & Most Secure Confirmation</li>
+                    <li><i class="ri-check-double-line"></i> Free Consultation & 24/7 Support</li>
                 </ul>
-                <a href="#about" class="get-started-btn scrollto" style="color: black;">49$ | ORDER NOW</a>
+                <br>
+                <a href="#">49$ | ORDER NOW</a>
             </div>
             <div class="col-lg-5" data-aos="fade-right">
                 <img src="assets/img/error1.png" class="img-fluid" alt="">
             </div>
         </div>
     </div>
-</section>
-<!-- ======= End Debugging Apps Section ======= -->
-
+</section><!-- End Debugging Apps section -->
 
 <!-- start information section -->
-<section id="info-2" class="info-2" style="background: black;">
+<section id="app-sup-info" class="app-sup-info">
     <div class="container text-center" data-aos="fade-up">
-        <h3 style="font-weight:800; color: #ffc451; font-style: italic; font-size: 35px; text-transform: uppercase;">Buy
-            Our Best Apps Integration Services​</h3>
-        <h5 style="font-weight:800; font-style: italic; color: white; text-transform: uppercase;">Our customer service
-            and engagement platform is powerful and flexible, and scales to meet the needs of any apps development
-            support.</h5>
+        <h3>Buy Our Best Apps Integration Services​</h3>
+        <h5>Our customer service and engagement platform is powerful and flexible, and scales to meet the needs of any apps development support.</h5>
     </div>
 </section><!-- End Information section -->
 
-
 <!-- ======= Pricing Section ======= -->
-<section id="pricing" class="pricing">
-
+<section id="app-sup-pricing" class="app-sup-pricing">
     <div class="container" data-aos="fade-up">
-
         <div class="row gy-4 justify-content-center" data-aos="fade-left">
-
             <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-                <div class="box" style="background: black;">
-                    <h3 style="color: #ffc451; font-weight: 800;">BEGINNER</h3>
-                    <div class="price" style="color: white;"><sup>$</sup>29</div><br>
+                <div class="box">
+                    <h3>BEGINNER</h3>
+                    <div class="price"><sup>$</sup>29</div><br>
                     <ul>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Stores App Submission</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1 Ad
-                            Network Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;Push
-                            Notification Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Splash Screen</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Logo, 1 Banner, 100% Professional</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Description ASO Friendly</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle"
-                                style="color: #ffc451;"></i>&nbsp;Content Rating Add</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Stores App Submission</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Ad Network Integration</li=>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Push Notification Integration</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Splash Screen</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Logo, 1 Banner, 100% Professional</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Description ASO Friendly</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Content Rating Add</li>
                     </ul>
-                    <a href="#" class="btn-buy">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
+                    <a href="#">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
                 </div>
             </div>
-
             <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-                <div class="box" style="background: black;">
+                <div class="box">
                     <span class="featured">Featured</span>
-                    <h3 style="color: #ffc451; font-weight: 800;">BUSINESS</h3>
-                    <div class="price" style="color: white;"><sup>$</sup>49</div><br>
+                    <h3>BUSINESS</h3>
+                    <div class="price"><sup>$</sup>49</div><br>
                     <ul>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;3
-                            Stores App Submission</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;2 Ad
-                            Network Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;Push
-                            Notification Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;2
-                            Splash Screen</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Logo, 2 Banner, 100% Professional</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;2
-                            Description ASO Friendly</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle"
-                                style="color: #ffc451;"></i>&nbsp;Content Rating Add</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;3 Stores App Submission</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;2 Ad Network Integration</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Push Notification Integration</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;2 Splash Screen</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Logo, 2 Banner, 100% Professional</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;2 Description ASO Friendly</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Content Rating Add</li>
                     </ul>
-                    <a href="#" class="btn-buy">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
+                    <a href="#">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
                 </div>
             </div>
-
             <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-                <div class="box" style="background: black;">
-                    <h3 style="color: #ffc451; font-weight: 800;">ADVANCE</h3>
-                    <div class="price" style="color: white;"><sup>$</sup>99</div><br>
+                <div class="box">
+                    <h3>ADVANCE</h3>
+                    <div class="price"><sup>$</sup>99</div><br>
                     <ul>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;3
-                            Stores App Submission</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;3 Ad
-                            Network Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;Push
-                            Notification Integration</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;3
-                            Splash Screen</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;1
-                            Logo, 3 Banner, 100% Professional</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle" style="color: #ffc451;"></i>&nbsp;3
-                            Description ASO Friendly</li>
-                        <li style="color: white;"><i class="bi bi-check2-circle"
-                                style="color: #ffc451;"></i>&nbsp;Content Rating Add</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;3 Stores App Submission</liyle=>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;3 Ad Network Integration</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Push Notification Integration</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;3 Splash Screen</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;1 Logo, 3 Banner, 100% Professional</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;3 Description ASO Friendly</li>
+                        <li><i class="bi bi-check2-circle"></i>&nbsp;Content Rating Add</li>
                     </ul>
-                    <a href="#" class="btn-buy">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
+                    <a href="#">Order Now &nbsp; <i class="bi bi-cart4"></i></a>
                 </div>
             </div>
-
-
         </div>
-
     </div>
-
 </section><!-- End Pricing Section -->
 
 
-
-
-
-
-
-<!-- End Debugging Apps Section  -->
 <?php require_once 'main/footer.php'; ?>
